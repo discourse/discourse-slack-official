045f9856c4e6d704840f848ec50f5cef4f38f100
Guo Xiang Tan <tgx_world@hotmail.com>
2017-04-05 10:10:26 +0800

    Move spec to the right place.

--- spec/integration/discourse_slack/slack_spec.rb
+++ spec/integration/discourse_slack/slack_spec.rb
@@ -9,7 +9,6 @@ describe 'Slack' do
 
   let(:first_post) { Fabricate(:post) }
   let(:topic) { Fabricate(:topic, posts: [first_post]) }
-  let(:delay) { SiteSetting.post_to_slack_window_secs.seconds }
 
   describe 'testing notification' do
     it 'should ping slack successfully' do
@@ -20,29 +19,4 @@ describe 'Slack' do
       expect(response).to be_success
     end
   end
-
-  context 'post' do
-
-    it 'should schedule a job for slack post' do
-      Timecop.freeze(Time.zone.now) do
-        Jobs.expects(:enqueue_in).with(delay, :notify_slack, has_entry(post_id: first_post.id))
-        DiscourseEvent.trigger(:post_created, first_post)
-      end
-    end
-
-    describe 'when plugin is not enabled' do
-      before do
-        SiteSetting.slack_enabled = false
-      end
-
-      it 'should not schedule a job for slack post' do
-        Timecop.freeze(Time.zone.now) do
-          Jobs.expects(:enqueue_in).with(delay, :notify_slack, has_entry(post_id: first_post.id)).never
-          DiscourseEvent.trigger(:post_created, first_post)
-        end
-      end
-    end
-
-  end
-
 end
---
+++ spec/lib/post_creator_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe PostCreator do
+  let(:first_post) { Fabricate(:post) }
+  let(:topic) { Fabricate(:topic, posts: [first_post]) }
+
+  before do
+    SiteSetting.slack_outbound_webhook_url = "https://hooks.slack.com/services/abcde"
+    SiteSetting.queue_jobs = true
+  end
+
+  describe 'when a post is created' do
+    describe 'when plugin is enabled' do
+      before do
+        SiteSetting.slack_enabled = true
+      end
+
+      it 'should schedule a job for slack post' do
+        Timecop.freeze do
+          post = PostCreator.new(topic.user,
+            raw: 'aaaaaaaaaaaaaaaaasdddddddddd sorry cat walked over my keyboard',
+            topic_id: topic.id
+          ).create!
+
+          job = Jobs::NotifySlack.jobs.last
+
+          expect(job['at'])
+            .to eq((Time.zone.now + SiteSetting.post_to_slack_window_secs.seconds).to_f)
+
+          expect(job['args'].first['post_id']).to eq(post.id)
+        end
+      end
+    end
+
+    describe 'when plugin is not enabled' do
+      before do
+        SiteSetting.slack_enabled = false
+      end
+
+      it 'should not schedule a job for slack post' do
+        PostCreator.new(topic.user,
+          raw: 'aaaaaaaaaaaaaaaaasdddddddddd sorry cat walked over my keyboard',
+          topic_id: topic.id
+        ).create!
+
+        expect(Jobs::NotifySlack.jobs).to eq([])
+      end
+    end
+  end
+end
